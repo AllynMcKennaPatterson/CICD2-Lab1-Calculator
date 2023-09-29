48478d1144e99cdd6e7fd374d6c4efba951a1c0d
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-09-30 00:36:28 +0100

    Created RestController

--- src/main/java/atu/cicd2/lab1calculator/CalculatorController.java
+++ src/main/java/atu/cicd2/lab1calculator/CalculatorController.java
@@ -1,4 +1,12 @@
 package atu.cicd2.lab1calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+    @GetMapping("/calculate")
+    public String calculate(int num1, int num2, String operation){
+
+    }
 }
