ca7bbbc232b95573e4bf7944a3046d04123f661c
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-09-30 00:10:01 +0100

    Initial commit

---
+++ src/main/java/atu/cicd2/lab1calculator/Lab1CalculatorApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab1calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/cicd2/lab1calculator/Lab1CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab1calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
