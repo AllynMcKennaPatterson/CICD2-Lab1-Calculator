34583fcaacc8e36cc3b8ac6f49985f41a0bf30a2
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-10-01 21:34:28 +0100

    Added Getters & Setters to Calculator class

--- src/main/java/atu/cicd2/lab1calculator/Calculator.java
+++ src/main/java/atu/cicd2/lab1calculator/Calculator.java
@@ -8,40 +8,70 @@ public class Calculator {
     private String operator;
     private float solution;
 
-    public Calculator(String operator, float solution) {
+    public Calculator() {
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
         this.operator = operator;
+    }
+
+    public float getSolution() {
+        return solution;
+    }
+
+    public void setSolution(float solution) {
         this.solution = solution;
     }
 
-    public Calculator CalculationResult(int num1, int num2, String operator){
+    public Calculator CalculateResult(){
         switch(operator){
             case "add":
             case "Add":
-                this.solution = num1 + num2;
+                this.solution = this.num1 + this.num2;
                 break;
             case "subtract":
             case "Subtract":
-                this.solution = num1 - num2;
+                this.solution = this.num1 - this.num2;
                 break;
             case "multiply":
             case "Multiply":
-                this.solution = num1 * num2;
+                this.solution = this.num1 * this.num2;
                 break;
             case "divide":
             case "Divide":
-                if( num1 == 0 || num2 == 0){
+                if( this.num1 == 0 || this.num2 == 0){
                     this.operator = "Error: Divide by zero";
                     this.solution = 0;
                 }
                 else {
-                    this.solution = (float) num1 / num2;
+                    this.solution = (float) this.num1 / this.num2;
                 }
                 break;
             default:
                 this.operator = "Error: Invalid operator entered";
                 this.solution = 0;
         }
-        return new Calculator(this.operator, this.solution);
+        return new Calculator();
 
     }
 }
