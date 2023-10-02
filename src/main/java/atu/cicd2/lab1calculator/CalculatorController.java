31ecf07f7329801a4948714e355171cb2ed3d04a
G00392765@atu.ie <G00392765@atu.ie>
2023-10-02 16:38:25 +0100

    Added "@RequestParam"

--- src/main/java/atu/cicd2/lab1calculator/CalculatorController.java
+++ src/main/java/atu/cicd2/lab1calculator/CalculatorController.java
@@ -1,12 +1,13 @@
 package atu.cicd2.lab1calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
     @GetMapping("/calculate")
-    public Calculator calculate(int num1, int num2, String operator){
+    public Calculator calculate(@RequestParam int num1,@RequestParam int num2,@RequestParam String operator){
         Calculator result = new Calculator(operator, 0);
         result.setOperator(operator);
 
