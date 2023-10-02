f20f3c1fc1873f5d533b6667a6262925ea69ab0c
G00392765@atu.ie <G00392765@atu.ie>
2023-10-02 16:07:32 +0100

    Removed num1 & num2 instance variables from Calculator class, they are now passed as arguments to the CalulateResult method

--- src/main/java/atu/cicd2/lab1calculator/Calculator.java
+++ src/main/java/atu/cicd2/lab1calculator/Calculator.java
@@ -1,40 +1,14 @@
 package atu.cicd2.lab1calculator;
 
-import org.springframework.expression.spel.ast.NullLiteral;
-
 public class Calculator {
-    private int num1;
-    private int num2;
     private String operator;
     private float solution;
-
-    public Calculator(int num1, int num2, String operator, float solution) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.operator = operator;
-        this.solution = solution;
-    }
 
     public Calculator(String operator, float solution) {
         this.operator = operator;
         this.solution = solution;
     }
 
-    public int getNum1() {
-        return num1;
-    }
-
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
-    }
 
     public String getOperator() {
         return operator;
@@ -52,35 +26,35 @@ public class Calculator {
         this.solution = solution;
     }
 
-    public Calculator CalculateResult(){
-        switch(operator){
+    public Calculator CalculateResult(int num1, int num2){
+        switch(this.operator){
             case "add":
             case "Add":
-                this.solution = this.num1 + this.num2;
+                this.solution = num1 + num2;
                 break;
             case "subtract":
             case "Subtract":
-                this.solution = this.num1 - this.num2;
+                this.solution = num1 - num2;
                 break;
             case "multiply":
             case "Multiply":
-                this.solution = this.num1 * this.num2;
+                this.solution = num1 * num2;
                 break;
             case "divide":
             case "Divide":
-                if( this.num1 == 0 || this.num2 == 0){
+                if( num1 == 0 || num2 == 0){
                     this.operator = "Error: Divide by zero";
                     this.solution = 0;
                 }
                 else {
-                    this.solution = (float) this.num1 / this.num2;
+                    this.solution = (float) num1 / num2;
                 }
                 break;
             default:
                 this.operator = "Error: Invalid operator entered";
                 this.solution = 0;
         }
-        return new Calculator(this.num1, this.num2, this.operator, this.solution);
+        return new Calculator(this.operator, this.solution);
 
     }
 }
