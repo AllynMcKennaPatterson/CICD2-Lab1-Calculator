b9c1da0b6d1b84031dfdcaba9ec9c93b9b4cdf4a
G00392765@atu.ie <G00392765@atu.ie>
2023-10-02 16:42:11 +0100

    Renamed file

---
+++ src/main/java/atu/cicd2/lab1calculator/CalculatorControllerRequestParam.java
@@ -0,0 +1,16 @@
+package atu.cicd2.lab1calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorControllerRequestParam {
+    @GetMapping("/calculate")
+    public Calculator calculate(@RequestParam int num1,@RequestParam int num2,@RequestParam String operator){
+        Calculator result = new Calculator(operator, 0);
+        result.setOperator(operator);
+
+        return result.CalculateResult(num1, num2);
+    }
+}
